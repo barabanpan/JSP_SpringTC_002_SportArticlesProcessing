607ab1b3f83ebd9abf5caac712202d8ed7f2bb06
Nataliia Dyshko <nataliia.dyshko@ukr.net>
2020-06-07 06:51:04 +0300

    Add POST and GET methods for /output
    Method GET returns message that says user shoud input data first.
    Method POST receives article from the form and shows table.

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,12 +1,27 @@
 from flask import Flask
-
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return "This thing is working!"
+def input():
+    return render_template('input.html', title="Input")
+
+
+@app.route('/output', methods=['GET'])
+def output_get():
+    return render_template(
+        'output.html', title="Output",
+         get_message="To see results input data first: ")
+
+
+@app.route('/output', methods=['POST'])
+def output_post():
+    article = request.form.get("article", "empty")
+    return render_template(
+        'output.html', title="Output", article=article)
 
 
 if __name__ == "__main__":
